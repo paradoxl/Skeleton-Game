c6cd82197becb9a8edf40718e78c30b52709e64a
Michael Evans <Michael.evans1775@outlook.com>
2023-06-02 19:22:26 -0600

    added idle animation

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
 import pygame
-import math
 width = 900
 height = 600
+i = 1
 # background = pygame.image.load()
 WIN = pygame.display.set_mode((width, height))
-playerIcon = pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_1.png")
-idlePlayer = [pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_1.png"), pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_2.png"),
-              pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_3.png"),pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_4.png"),
-              pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_5.png"),pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_6.png"),
-              pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_7.png"),pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_8.png"),
-              pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_9.png"),pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_10.png")
-              ,pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_11.png"),pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_12.png")]
 playerHeight = 400
 playerWidth = 200
-playerIcon = pygame.transform.scale(playerIcon, (playerHeight, playerWidth))
+# playerIcon = pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_1.png")
+idlePlayer = [pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_1.png"),(playerHeight,playerWidth)), pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_2.png"),(playerHeight,playerWidth)),
+              pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_3.png"),(playerHeight,playerWidth)),pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_4.png"),(playerHeight,playerWidth)),
+              pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_5.png"),(playerHeight,playerWidth)),pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_6.png"),(playerHeight,playerWidth)),
+              pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_7.png"),(playerHeight,playerWidth)),pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_8.png"),(playerHeight,playerWidth)),
+              pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_9.png"),(playerHeight,playerWidth)),pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_10.png"),(playerHeight,playerWidth))
+              ,pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_11.png"),(playerHeight,playerWidth)),pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_12.png"),(playerHeight,playerWidth))]
+
+
+# playerIcon = pygame.transform.scale(playerIcon, (playerHeight, playerWidth))
 background_image = pygame.transform.scale(pygame.image.load("assets/NightForest/Layers/Image.png"),(width,height))
 moving = False
 
 
 def main():
     player = pygame.Rect(100,300, playerWidth,playerHeight)
-    clock = pygame.time.Clock
+    clock = pygame.time.Clock()
+    clock.tick(60)
     running = True
     while running:
         for event in pygame.event.get():
@@ -33,19 +36,31 @@ def main():
                 
             
 def draw(player):
+    clock = pygame.time.Clock()
+    clock.tick(10)
     WIN.blit(background_image,(0,0))
-    # WIN.blit(playerIcon,(player.x,player.y))
-    for i in idlePlayer:
-        WIN.blit(i,(player.x, player.y))
+    global i
+    global width
+    global height
+    if i >= 12:
+        i = 1
+    value = idlePlayer[i]
+    WIN.blit(value,(player.x,player.y))
     pygame.display.update()
+    i += 1
 
 def playerMovement(kp,player):
     if kp[pygame.K_LEFT]:
-        player.x -= 1
-
-
+        playerMoving = True
+        player.x -= 5
     if kp[pygame.K_RIGHT]:
-        player.x += 1
+        playerMoving = True
+        player.x += 5
+    if kp[pygame.K_SPACE]:
+        playerJumping = True
+        player.y -=1
+        
+
               
 
 
