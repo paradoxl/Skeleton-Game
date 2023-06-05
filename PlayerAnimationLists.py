b6a02f227ab54dcac065489e273810ed30535d22
Michael Evans <Michael.evans1775@outlook.com>
2023-06-05 17:27:05 -0600

    wip running animation

--- PlayerAnimationLists.py
+++ PlayerAnimationLists.py
@@ -1,16 +1,8 @@
-import math
 import pygame
-width = 900
-height = 600
-i = 1
-WIN = pygame.display.set_mode((width, height))
-border = pygame.Rect((width,0,10,height))
-BLACK = (0, 0, 0)
-playerHeight = 400
 playerWidth = 200
-playerMoving = False
+playerHeight =400
 
-# playerIcon = pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_1.png")
+
 idlePlayer = [pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_1.png"),(playerHeight,playerWidth)), pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_2.png"),(playerHeight,playerWidth)),
               pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_3.png"),(playerHeight,playerWidth)),pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_4.png"),(playerHeight,playerWidth)),
               pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_5.png"),(playerHeight,playerWidth)),pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_6.png"),(playerHeight,playerWidth)),
@@ -23,92 +15,3 @@ playerRunningRight = [pygame.transform.scale(pygame.image.load("assets/Elemental
                       pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/run/run_5.png"),(playerHeight,playerWidth)),pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/run/run_6.png"),(playerHeight,playerWidth)),
                       pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/run/run_7.png"),(playerHeight,playerWidth)),pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/run/run_8.png"),(playerHeight,playerWidth)),
                       pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/run/run_9.png"),(playerHeight,playerWidth)),pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/run/run_10.png"),(playerHeight,playerWidth))]
-
-# playerIcon = pygame.transform.scale(playerIcon, (playerHeight, playerWidth))
-background_image = pygame.transform.scale(pygame.image.load("assets/NightForest/Layers/Image.png"),(width,height))
-moving = False
-
-#Scrolling background
-scroll = 0
-scrollThreshRight = 600
-scrollThreshLeft = 50
-tiles = math.ceil(width / background_image.get_width()) + 1
-
-def main():
-    player = pygame.Rect(100,300, playerWidth,playerHeight)
-    clock = pygame.time.Clock()
-    clock.tick(60)
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-        kp = pygame.key.get_pressed()
-        playerMovement(kp,player)
-        draw(player)
-           
-                
-            
-def draw(player):
-    global scroll
-    global playerMoving
-    clock = pygame.time.Clock()
-    clock.tick(10)
-#the background scrolls. need to have it do this only when player goes off edge of screen.
-    counter = 0
-    while (counter < tiles):
-        WIN.blit(background_image, (background_image.get_width() * counter + scroll, 0))
-        counter += 1
-    # scroll -= 6
-    if abs(scroll) > background_image.get_width():
-        scroll = 0
-    global i
-    global width
-    global height
-
-    #Makes the player idle animation
-    if not playerMoving:
-        if i >= 12:
-            i = 1
-        value = idlePlayer[i]
-        WIN.blit(value,(player.x,player.y))
-        pygame.display.update()
-        i += 1
-
-    if playerMoving:
-        if i > 9:
-            i = 0
-        if i >= 9:
-            i = 1
-            running = playerRunningRight[i]
-            WIN.blit(running,(player.x,player.y))
-            pygame.display.update()
-            i += 1
-
-def playerMovement(kp,player):
-    global scroll
-    global playerMoving
-
-    if kp[pygame.K_LEFT] and player.x - 5 > 0:
-        player.x -= 10
-        if player.x < scrollThreshLeft:
-            scroll += 30
-            player.x += 10
-
-    if kp[pygame.K_RIGHT]:
-        player.x += 10
-        if player.x > scrollThreshRight:
-            scroll -= 30
-            player.x -= 10
-        playerMoving = True
-
-    if kp[pygame.K_SPACE]:
-        playerJumping = True
-        player.y -=1
-        
-
-              
-
-
-
-main()
--- animationLists.py
+++
@@ -1,6 +0,0 @@
-import pygame
-width = 200
-height =400
-idle1 = pygame.transform.scale(pygame.image.load
-                               ("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_1.png")
-                               ,(width,height))
