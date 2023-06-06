265445a6bf086373e71473b36fa4fccffe5dc268
Michael Evans <Michael.evans1775@outlook.com>
2023-06-06 11:56:21 -0600

    wip

--- main.py
+++ main.py
@@ -1,29 +1,14 @@
 import math
 import pygame
+import PlayerAnimationLists
+import player
 width = 900
 height = 600
+playerMoving = False # Code debt in the making
 i = 1
 WIN = pygame.display.set_mode((width, height))
-border = pygame.Rect((width,0,10,height))
-BLACK = (0, 0, 0)
 playerHeight = 400
 playerWidth = 200
-playerMoving = False
-
-# playerIcon = pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_1.png")
-idlePlayer = [pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_1.png"),(playerHeight,playerWidth)), pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_2.png"),(playerHeight,playerWidth)),
-              pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_3.png"),(playerHeight,playerWidth)),pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_4.png"),(playerHeight,playerWidth)),
-              pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_5.png"),(playerHeight,playerWidth)),pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_6.png"),(playerHeight,playerWidth)),
-              pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_7.png"),(playerHeight,playerWidth)),pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_8.png"),(playerHeight,playerWidth)),
-              pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_9.png"),(playerHeight,playerWidth)),pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_10.png"),(playerHeight,playerWidth))
-              ,pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_11.png"),(playerHeight,playerWidth)),pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/idle/idle_12.png"),(playerHeight,playerWidth))]
-
-playerRunningRight = [pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/run/run_1.png"),(playerHeight,playerWidth)),pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/run/run_2.png"),(playerHeight,playerWidth)),
-                      pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/run/run_3.png"),(playerHeight,playerWidth)),pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/run/run_4.png"),(playerHeight,playerWidth)),
-                      pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/run/run_5.png"),(playerHeight,playerWidth)),pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/run/run_6.png"),(playerHeight,playerWidth)),
-                      pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/run/run_7.png"),(playerHeight,playerWidth)),pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/run/run_8.png"),(playerHeight,playerWidth)),
-                      pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/run/run_9.png"),(playerHeight,playerWidth)),pygame.transform.scale(pygame.image.load("assets/Elementals_Leaf_ranger_Free_v1.0/animations/PNG/run/run_10.png"),(playerHeight,playerWidth))]
-
 # playerIcon = pygame.transform.scale(playerIcon, (playerHeight, playerWidth))
 background_image = pygame.transform.scale(pygame.image.load("assets/NightForest/Layers/Image.png"),(width,height))
 moving = False
@@ -33,9 +18,12 @@ scroll = 0
 scrollThreshRight = 600
 scrollThreshLeft = 50
 tiles = math.ceil(width / background_image.get_width()) + 1
+player1 = player.Player(playerHeight, playerWidth, 10, 10, 100, 300, False, False, False)
+
 
 def main():
-    player = pygame.Rect(100,300, playerWidth,playerHeight)
+    print(player1)
+    player_box = pygame.Rect(100,300, playerWidth,playerHeight)
     clock = pygame.time.Clock()
     clock.tick(60)
     running = True
@@ -44,43 +32,43 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
         kp = pygame.key.get_pressed()
-        playerMovement(kp,player)
-        draw(player)
+        playerMovement(kp,player_box)
+        draw(player_box,kp)
            
                 
             
-def draw(player):
+def draw(player,kp):
     global scroll
-    global playerMoving
     clock = pygame.time.Clock()
     clock.tick(10)
-#the background scrolls. need to have it do this only when player goes off edge of screen.
     counter = 0
     while (counter < tiles):
         WIN.blit(background_image, (background_image.get_width() * counter + scroll, 0))
         counter += 1
-    # scroll -= 6
     if abs(scroll) > background_image.get_width():
         scroll = 0
+
+
+
     global i
-    global width
-    global height
+    # global width
+    # global height
 
     #Makes the player idle animation
-    if not playerMoving:
+    if not kp[pygame.K_LEFT] and kp[pygame.K_RIGHT]:
         if i >= 12:
             i = 1
-        value = idlePlayer[i]
+        value = PlayerAnimationLists.idlePlayer[i]
         WIN.blit(value,(player.x,player.y))
         pygame.display.update()
         i += 1
 
-    if playerMoving:
+    if kp[pygame.K_RIGHT]:
         if i > 9:
             i = 0
         if i >= 9:
             i = 1
-            running = playerRunningRight[i]
+            running = PlayerAnimationLists.playerRunningRight[i]
             WIN.blit(running,(player.x,player.y))
             pygame.display.update()
             i += 1
@@ -96,11 +84,14 @@ def playerMovement(kp,player):
             player.x += 10
 
     if kp[pygame.K_RIGHT]:
+        player1.moving_left = False
+        player1.moving_right = True
+        player1.idle = False
         player.x += 10
         if player.x > scrollThreshRight:
             scroll -= 30
             player.x -= 10
-        playerMoving = True
+        # playerMoving = True
 
     if kp[pygame.K_SPACE]:
         playerJumping = True
--- player.py
+++ player.py
@@ -3,32 +3,38 @@ import pygame.display
 import PlayerAnimationLists
 
 
-class player():
-    def __init__(self, player_height, player_width, health, velocity, x,y):
+class Player():
+    def __init__(self, player_height, player_width, health, velocity, x,y,
+                 moving_right, moving_left,idle):
         self.player_height = player_height
         self.player_width = player_width
         self.health = health
         self.velocity = velocity
         self.x = x
         self.y = y
+        self.moving_right = moving_right
+        self.moving_left = moving_left
+        self.idle = idle
 
-    def drawPlayer(self, kp, moving, moving_right, moving_left, hit_box):
-        if not moving:
-            if i >= 12:
-                i = 1
-                idle = PlayerAnimationLists.idlePlayer[i]
-                # How do i blit outside of the main file.
-                # WIN.blit(idle, (player.x, player.y))
-                pygame.display.update()
-                i +=1
 
-        if moving:
-            if moving_right:
-                if i > 9:
-                    i = 1
-                    running_right = PlayerAnimationLists.playerRunningRight[i]
-                    #WIN.blit(running_right(player.x,player.y))
-                    pygame.display.update()
-                    i += 1
 
-    
+
+    # def drawPlayer(self, kp, moving, moving_right, moving_left, hit_box):
+    #     if not moving:
+    #         if i >= 12:
+    #             i = 1
+    #             idle = PlayerAnimationLists.idlePlayer[i]
+    #             # How do i blit outside of the main file.
+    #             # WIN.blit(idle, (player.x, player.y))
+    #             pygame.display.update()
+    #             i +=1
+    #
+    #     if moving:
+    #         if moving_right:
+    #             if i > 9:
+    #                 i = 1
+    #                 running_right = PlayerAnimationLists.playerRunningRight[i]
+    #                 #WIN.blit(running_right(player.x,player.y))
+    #                 pygame.display.update()
+    #                 i += 1
+    #
