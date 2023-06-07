2aabb3f8a962700696607582a55836da4eab953b
Michael Evans <Michael.evans1775@outlook.com>
2023-06-07 13:55:12 -0600

    refactor complete

--- main.py
+++ main.py
@@ -2,127 +2,17 @@ import math
 import pygame
 import PlayerAnimationLists
 import player
-
-
-# width = 900pi
-# height = 600
-# playerMoving = False # Code debt in the making
-# WIN = pygame.display.set_mode((width, height))
-# playerHeight = 400
-# playerWidth = 200
-# # playerIcon = pygame.transform.scale(playerIcon, (playerHeight, playerWidth))
-# moving = False
-#
-# #Scrolling background
-# scroll = 0
-# scrollThreshRight = 600
-# scrollThreshLeft = 50
-# tiles = math.ceil(width / background_image.get_width()) + 1
-# player1 = player.Player(playerHeight, playerWidth, 10, 10, 100, 300, False, False, False)
-#
-#
-# #player movemnet vvariables
-# idle_counter = 0
-# running_right_counter = 0
-#
-# def main():
-#     print(player1)
-#     player_box = pygame.Rect(100,300, playerWidth,playerHeight)
-#     clock = pygame.time.Clock()
-#     clock.tick(60)
-#     running = True
-#     while running:
-#         for event in pygame.event.get():
-#             if event.type == pygame.QUIT:
-#                 running = False
-#         kp = pygame.key.get_pressed()
-#         playerMovement(kp,player_box)
-#         draw(player_box,kp)
-#
-#
-#
-# def draw(player,kp):
-#     global scroll
-#     clock = pygame.time.Clock()
-#     clock.tick(10)
-#     counter = 0
-#     while (counter < tiles):
-#         WIN.blit(background_image, (background_image.get_width() * counter + scroll, 0))
-#         counter += 1
-#     if abs(scroll) > background_image.get_width():
-#         scroll = 0
-#
-#
-#     global idle_counter
-#     # Makes the player idle animation
-#     if not moving:
-#         if idle_counter >= 12:
-#             idle_counter = 1
-#         value = PlayerAnimationLists.idlePlayer[idle_counter]
-#         WIN.blit(value,(player.x,player.y))
-#         pygame.display.update()
-#         idle_counter += 1
-#
-#     global running_right_counter
-#     if moving:
-#         if running_right_counter > 9:
-#             running_right_counter = 0
-#         if running_right_counter >= 9:
-#             running_right_counter = 1
-#             running = PlayerAnimationLists.playerRunningRight[running_right_counter]
-#             WIN.blit(running,(player.x,player.y))
-#             pygame.display.update()
-#             running_right_counter += 1
-#
-# def playerMovement(kp,player):
-#     global scroll
-#     global playerMoving
-#
-#     if kp[pygame.K_LEFT] and player.x - 5 > 0:
-#         player.x -= 10
-#         if player.x < scrollThreshLeft:
-#             scroll += 30
-#             player.x += 10
-#
-#     if kp[pygame.K_RIGHT]:
-#         player1.moving_left = False
-#         player1.moving_right = True
-#         player1.idle = False
-#         player.x += 10
-#         if player.x > scrollThreshRight:
-#             scroll -= 30
-#             player.x -= 10
-#         # playerMoving = True
-#
-#     if kp[pygame.K_SPACE]:
-#         playerJumping = True
-#         player.y -=1
-#
-#
-#
-#
-#
-#
-# main()
-
-
-
-
-
-
-
-
-
-
-
-#####################################################################3
-#Refactor this garbage fire
 #####################
 # issues
 # game does not exit immediately
-
 ####################
 
+####################
+# Notes
+# Direction facing is a boolean.
+# False is facing left
+# True is facing right
+####################
 
 # pygame stuffs
 screen_width = 900
@@ -148,13 +38,12 @@ idle_counter = 1
 moving_right_counter = 1
 moving_left_counter = 1
 attack_counter = 1
-# Direction facing is a boolean.
-# False is facing left
-# True is facing right
+
 player_one = player.Player(player_height, player_width, player_health, player_velocity, player_starting_pos_x,
                            player_starting_pos_y, False, False, False,False,True)
 
 player_hitbox = pygame.Rect(player_one.x, player_one.y, player_one.player_width, player_one.player_height)
+
 
 def main():
     # variables for scrolling background
@@ -179,6 +68,7 @@ def main():
         draw_player(keys_pressed,player_hitbox,player_one)
 
         pygame.display.update()
+
 
 def draw_player(keys_pressed, player_hitbox, player_one):
     global idle_counter
@@ -246,6 +136,7 @@ def draw_background(counter,scroll,tiles):
     if abs(scroll) > background_image.get_width():
         scroll = 0
 
+
 def player_movement(keys_pressed,player_one,player_hitbox):
     if keys_pressed[pygame.K_RIGHT]:
         player_one.x += 10
@@ -265,8 +156,7 @@ def player_movement(keys_pressed,player_one,player_hitbox):
         player_one.idle = False
         player_one.moving_right = False
         player_one.moving_left = False
-    print(player_one.moving_right, "right")
-    print(player_one.moving_left,"left")
 
 
-main()
+if __name__ == '__main__':
+          main()
