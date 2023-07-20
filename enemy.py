0a5dee5c4a5b6fe9d0e474c056f715c54822d3a5
Michael Evans <Michael.evans1775@outlook.com>
2023-07-19 19:46:31 -0600

    add enemy class

---
+++ enemy.py
@@ -0,0 +1,14 @@
+class Enemy():
+    def __init__(self,enemy_height, enemy_width,health,velocity,x_pos,y_pos,moving_right,moving_left,idle,attacking,
+                 direction_facing):
+        self.enemy_height = enemy_height
+        self.enemy_width = enemy_width
+        self.health = health
+        self.velocity = velocity
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.moving_right = moving_right
+        self.moving_left = moving_left
+        self.idle = idle
+        self.attacking = attacking
+        self.direction_facing = direction_facing
